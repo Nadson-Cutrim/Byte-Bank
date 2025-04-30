d3aa2aec626752c55ccc8db99e3006fcabb38693
Nadson Rodrigues Cutrim Junior <nadsoncutrimjunior@aluno.uema.br>
2025-04-29 23:38:22 -0300

    Refatoração com TypeScript

--- js/bytebank.js
+++ js/bytebank.js
@@ -1,37 +1,37 @@
-let saldo = 3000;
-const elementoSaldo = document.querySelector(".saldo-valor .valor");
-elementoSaldo.textContent = saldo;
-
-
-const elementoFormulario = document.querySelector(".block-nova-transacao form");
+var saldo = 3000;
+var elementoSaldo = document.querySelector(".saldo-valor .valor");
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+var elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener('submit', function (event) {
-event.preventDefault(); // Evita o envio do formulário
-if(!elementoFormulario.checkValidity()) {
-    alert("Preencha todos os campos da transação corretamente!");
-    return;
-}
-const inputTipodeTrasacao = elementoFormulario.querySelector("#tipoTransacao");
-const inputValor = elementoFormulario.querySelector("#valor");
-const inputData = elementoFormulario.querySelector("#data");
-
-let tipoTransacao = inputTipodeTrasacao.value;
-let valor = inputValor.value;
-let data = inputData.value;
-
-if (tipoTransacao === "Deposito") {
-    saldo += valor;
-}else if (tipoTransacao === "Saque" || tipoTransacao === "Pagamento de Boleto") {
-    saldo -= valor;
-}else{
-    alert("Tipo de transação inválido!");
-    return;
-}
-
-const novaTransacao = {
-    tipoTransacao: tipoTransacao,
-    valor: valor,
-    data: data
-}
-console.log(novaTransacao);
-elementoFormulario.reset(); // Limpa os campos do formulário
-})
+    event.preventDefault(); // Evita o envio do formulário
+    if (!elementoFormulario.checkValidity()) {
+        alert("Preencha todos os campos da transação corretamente!");
+        return;
+    }
+    var inputTipoTrasacao = elementoFormulario.querySelector("#tipoTransacao");
+    var inputValor = elementoFormulario.querySelector("#valor");
+    var inputData = elementoFormulario.querySelector("#data");
+    var tipoTransacao = inputTipoTrasacao.value;
+    var valor = inputValor.valueAsNumber;
+    var data = new Date(inputData.value);
+    if (tipoTransacao === "Depósito") {
+        saldo += valor;
+    }
+    else if (tipoTransacao === "Transferência" || tipoTransacao === "Pagamento de Boleto") {
+        saldo -= valor;
+    }
+    else {
+        alert("Tipo de transação inválido!");
+        return;
+    }
+    elementoSaldo.textContent = saldo.toString(); // Atualiza o saldo na tela
+    var novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    };
+    console.log(novaTransacao);
+    elementoFormulario.reset(); // Limpa os campos do formulário
+});
---
+++ js/bytebank.ts
@@ -0,0 +1,38 @@
+let saldo = 3000;
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+if(elementoSaldo != null){
+    elementoSaldo.textContent = saldo.toString();
+}
+
+const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
+elementoFormulario.addEventListener('submit', function (event) {
+event.preventDefault(); // Evita o envio do formulário
+if(!elementoFormulario.checkValidity()) {
+    alert("Preencha todos os campos da transação corretamente!");
+    return;
+}
+const inputTipoTrasacao = elementoFormulario.querySelector("#tipoTransacao") as HTMLSelectElement;
+const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement;
+const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement;
+
+let tipoTransacao: string = inputTipoTrasacao.value;
+let valor: number = inputValor.valueAsNumber;
+let data: Date = new Date(inputData.value);
+
+if (tipoTransacao === "Depósito") {
+    saldo += valor;
+}else if (tipoTransacao === "Transferência" || tipoTransacao === "Pagamento de Boleto") {
+    saldo -= valor;
+}else{
+    alert("Tipo de transação inválido!");
+    return;
+}
+elementoSaldo.textContent = saldo.toString(); // Atualiza o saldo na tela
+const novaTransacao = {
+    tipoTransacao: tipoTransacao,
+    valor: valor,
+    data: data
+}
+console.log(novaTransacao);
+elementoFormulario.reset(); // Limpa os campos do formulário
+})
